f005701c01509cdbc43b92a2fae07580d68f5ad3
My Name <lulu.agustin105319@students.unila.ac.id>
2024-07-03 23:36:46 +0700

    add detail job

--- controllers/JobRecruitmentController.js
+++ controllers/JobRecruitmentController.js
@@ -1,16 +1,15 @@
 const axios = require("axios");
 
 module.exports = {
-
   viewJobRecruitment: async (req, res) => {
     let jobrecruitmentResponse = await axios.get(
-      "https://jsonplaceholder.typicode.com/posts"
+      `${process.env.baseUrl}/admin/jobrecruitment/lists`
     );
 
-    let jobrecruitmentData = jobrecruitmentResponse.data;
+    let responseData = jobrecruitmentResponse.data;
 
     res.render("jobrecruit/job", {
-      jobrecruitmentData,
+      jobrecruitmentData : responseData.jobRecruitment,
     });
   },
 
@@ -26,17 +25,36 @@ module.exports = {
     });
   },
 
+  detailJobRecruitment: async (req, res) => {
+    try {
+      const { id } = req.params;
+
+      const jobrecruitmentResponse = await axios.get(
+        `${process.env.baseUrl}/admin/${id}/jobrecruitment/detail`
+      );
+
+      const responseData = jobrecruitmentResponse.data;
+
+      res.render("jobrecruit/detail_job", {
+        jobrecruitmentData: responseData.data,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Terjadi kesalahan pada server");
+    }
+  },
+
   viewJobRecruitmentCategory: async (req, res) => {
-  let jobrecruitmentcategoryResponse = await axios.get(
-    `${process.env.baseUrl}/admin/jobcategory/lists`,
-  );
+    let jobrecruitmentcategoryResponse = await axios.get(
+      `${process.env.baseUrl}/admin/jobcategory/lists`
+    );
 
-  let responseData = jobrecruitmentcategoryResponse.data;
+    let responseData = jobrecruitmentcategoryResponse.data;
 
-  res.render("jobrecruit/job_category", {
-    jobrecruitmentcategoryData : responseData.data,
-  });
-},
+    res.render("jobrecruit/job_category", {
+      jobrecruitmentcategoryData: responseData.data,
+    });
+  },
 
   viewPostJobRecruitmentCategory: async (req, res) => {
     let jobrecruitmentcategoryResponse = await axios.get(
@@ -53,14 +71,13 @@ module.exports = {
   detailJobRecruitmentCategory: async (req, res) => {
     try {
       const { id } = req.params;
-  
-      // Panggil API dengan parameter id
+
       const jobrecruitmentcategoryResponse = await axios.get(
-        `${process.env.baseUrl}/admin/${id}/jobcategory/detail`,
+        `${process.env.baseUrl}/admin/${id}/jobcategory/detail`
       );
-  
+
       const responseData = jobrecruitmentcategoryResponse.data;
-  
+
       res.render("jobrecruit/detail_job_category", {
         jobrecruitmentcategoryData: responseData.data,
       });
@@ -69,5 +86,4 @@ module.exports = {
       res.status(500).send("Terjadi kesalahan pada server");
     }
   },
-
 };
--- routes/jobrecruitment.js
+++ routes/jobrecruitment.js
@@ -2,8 +2,8 @@ const router = require("express").Router();
 const jobrecruitmentControllers = require("../controllers/JobRecruitmentController");
 
 router.get('/jobrecruit', jobrecruitmentControllers.viewJobRecruitment);
-
 router.get('/jobrecruit/create', jobrecruitmentControllers.viewPostJobRecruitment);
+router.get('/jobrecruit/detail/:id', jobrecruitmentControllers.detailJobRecruitment);
 
 router.get('/jobrecruit/category', jobrecruitmentControllers.viewJobRecruitmentCategory);
 router.get('/jobrecruit/category/create', jobrecruitmentControllers.viewPostJobRecruitmentCategory);
