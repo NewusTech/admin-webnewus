485c12d406421d42fdaeae0599c2953cb1073ffb
My Name <lulu.agustin105319@students.unila.ac.id>
2024-07-04 23:39:13 +0700

    integrasi service

--- controllers/ServiceController.js
+++ controllers/ServiceController.js
@@ -1,10 +1,11 @@
 const axios = require("axios");
+const FormData = require("form-data");
 
 module.exports = {
 
   viewService: async (req, res) => {
     let serviceResponse = await axios.get(
-      "https://api-services.newus.id/api/admin/service/get"
+      `${process.env.baseUrl}/admin/service/get`
     );
 
     let responseData = serviceResponse.data;
@@ -32,7 +33,7 @@ module.exports = {
   
       // Panggil API dengan parameter slug
       const serviceResponse = await axios.get(
-        `https://api-services.newus.id/api/admin/${id}/service/detail`
+        `${process.env.baseUrl}/admin/${id}/service/detail`
       );
   
       const responseData = serviceResponse.data;
@@ -43,6 +44,40 @@ module.exports = {
     } catch (error) {
       console.error(error);
       res.status(500).send("Terjadi kesalahan pada server");
+    }
+  },
+
+  createService: async (req, res) => {
+    try {
+      const token = req.body.token;
+      const formData = new FormData();
+      const imageFile = req.files.image;
+
+      formData.append("title", req.body.title);
+      formData.append("description", req.body.description);
+      formData.append("image", imageFile.data, imageFile.name);
+      formData.append("createdAt", new Date().toISOString());
+      formData.append("updatedAt", new Date().toISOString());
+
+      const response = await axios.post(
+        `${process.env.baseUrl}/admin/blog/create`,
+        formData,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'multipart/form-data'
+          },
+        }
+      );
+
+      if (response.status === 201) {
+        res.render('service/add_service', { successMessage: 'Service created successfully' });
+      } else {
+        res.render('service/add_service', { errorMessage: 'Failed to create service' });
+      }
+    } catch (error) {
+      console.error('Error creating service:', error);
+      res.render('service/add_service', { errorMessage: 'Error creating service' });
     }
   },
   
--- routes/service.js
+++ routes/service.js
@@ -3,6 +3,7 @@ const serviceControllers = require("../controllers/ServiceController");
 
 router.get('/service', serviceControllers.viewService);
 router.get('/service/create', serviceControllers.viewPostService);
+router.post('/service/create', serviceControllers.createService);
 router.get('/admin/:id/service/detail', serviceControllers.detailService);
 
 module.exports = router;
