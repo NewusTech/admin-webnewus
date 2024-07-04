6240b56f5c6216fb68a457fdf6894c7a048a4f2c
My Name <lulu.agustin105319@students.unila.ac.id>
2024-07-04 23:39:36 +0700

    get company

--- controllers/CompanyController.js
+++ controllers/CompanyController.js
@@ -14,6 +14,7 @@ module.exports = {
     });
   },
 
+
   viewPostCompany: async (req, res) => {
     let companyResponse = await axios.get(
       "https://jsonplaceholder.typicode.com/posts"
@@ -24,6 +25,58 @@ module.exports = {
     res.render("company/add_company", {
       companyData,
     });
+  },
+
+  createCompany: async (req, res) => {
+    try {
+      const {
+        description,
+        vision,
+        mission,
+        token,
+      } = req.body;
+      const response = await axios.post(`${process.env.baseUrl}/admin/aboutcompany/create`, {
+        description,
+        vision,
+        mission,
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString(),
+        token,
+      }, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        });
+
+        if (response.status === 201) {
+          res.render('company/add_company', { successMessage: 'company created successfully' });
+        } else {
+          res.render('company/add_company', { errorMessage: 'Failed to create company' });
+        }
+    } catch (error) {
+      res.render('company/add_company', { errorMessage: 'Error creating company' });
+    }
+  },
+
+  detailCompany: async (req, res) => {
+    try {
+      const { slug } = req.params;
+  
+      // Panggil API dengan parameter slug
+      const companyResponse = await axios.get(
+        `${process.env.baseUrl}`
+      );
+  
+      const responseData = companyResponse.data;
+  
+      res.render("company/detail_company", {
+        companyData: responseData.data,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Terjadi kesalahan pada server");
+    }
   },
   
 };
