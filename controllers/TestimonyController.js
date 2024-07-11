8bda4f395fdc743c1d4b34ca24e1871bfca87315
My Name <lulu.agustin105319@students.unila.ac.id>
2024-07-11 17:00:31 +0700

    integrasi create testimony

--- controllers/TestimonyController.js
+++ controllers/TestimonyController.js
@@ -14,5 +14,52 @@ module.exports = {
     });
   },
 
+  viewPostTestimony: async (req, res) => {
+    let testimonyResponse = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+
+    let testimonyData = testimonyResponse.data;
+
+    res.render("testimony/add_testimony", {
+      testimonyData,
+    });
+  },
+
+  createTestimony: async (req, res) => {
+    try {
+      const token = req.body.token;
+      const formData = new FormData();
+      const imageFile = req.files.image;
+
+      formData.append("name", req.body.name);
+      formData.append("testimony", req.body.testimony);
+      formData.append("rating", req.body.rating);
+      formData.append("image", imageFile.data, imageFile.name);
+      formData.append("createdAt", new Date().toISOString());
+      formData.append("updatedAt", new Date().toISOString());
+
+      const response = await axios.post(
+        `${process.env.baseUrl}/admin/testimony/new-testimony`,
+        formData,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'multipart/form-data'
+          },
+        }
+      );
+
+      if (response.status === 201) {
+        res.render('testimony/add_testimony', { successMessage: 'Testimony created successfully' });
+      } else {
+        res.render('testimony/add_testimony', { errorMessage: 'Failed to create testimony' });
+      }
+    } catch (error) {
+      console.error('Error creating testimony:', error);
+      res.render('testimony/add_testimony', { errorMessage: 'Error creating testimony' });
+    }
+  },
+
   
 };
