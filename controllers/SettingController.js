f6b95534fe3db66c5731e483f2bbf2833220eb06
My Name <lulu.agustin105319@students.unila.ac.id>
2024-07-26 16:55:25 +0700

    solve bugs recomendation

--- controllers/SettingController.js
+++ controllers/SettingController.js
@@ -38,29 +38,118 @@ module.exports = {
     }, catch (error) {
       console.error('Error updating setting data:', error);
       res.status(500).send('Internal Server Error');
-    },
-
-  viewSosmed: async (req, res) => {
-    let settingResponse = await axios.get(
-      "https://jsonplaceholder.typicode.com/posts"
-    );
-
-    let settingData = settingResponse.data;
-
-    res.render("socialmedia", {
-      settingData,
-    });
   },
 
-  viewSeo: async (req, res) => {
-    let settingResponse = await axios.get(
-      "https://jsonplaceholder.typicode.com/posts"
-    );
+  viewSosmed: async (req, res) => {
+      try {
+    
+        // const {id} = req.params;
 
-    let settingData = settingResponse.data;
+        // Panggil API dengan parameter id
+        const settingResponse = await axios.get(
+          `${process.env.baseUrl}/admin/socialmedia/get`
+        );
+    
+        let responseData = settingResponse.data;
+  
+        res.render("socialmedia", {
+          dataSetting : responseData.data
+        });
+      } catch (error) {
+        console.error('ERRRRRRR : ',error);
+        res.status(500).send("Terjadi kesalahan pada server");
+      }
+    },
+    
+  updateSosmed: async (req, res) => {
+      try {
+        const id = req.params.id;
+        const { link, token } = req.body;
+    
+        let settingResponse = await axios.put(
+          `${process.env.baseUrl}/admin/${id}/socialmedia/update`,
+          { link },
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+              "Content-Type": "application/json",
+            }
+          }
+        );
+    
+        if (settingResponse.status === 200) {
+          res.render('socialmedia', {
+            successMessage: 'Social media updated successfully',
+            settingData: settingResponse.data.data 
+          });
+        } else {
+          res.render('socialmedia', {
+            errorMessage: 'Failed to updated setting tag',
+            settingData: req.body
+          });
+        }
+      } catch (error) {
+        console.error('Error updating tag:', error.message);
+        res.render('socialmedia', {
+          errorMessage: 'Internal Server Error',
+          settingData: req.body
+        });
+      }
+    },
 
-    res.render("seo", {
-      settingData,
-    });
-  }
+  viewSEO: async (req, res) => {
+    try {
+  
+      // Panggil API dengan parameter id
+      const settingResponse = await axios.get(
+        `${process.env.baseUrl}/admin/1/seo/detail`,
+      );
+  
+      let responseData = settingResponse.data;
+
+      res.render("seo", {
+        dataSetting : responseData.data
+      });
+    } catch (error) {
+      console.error('ERRRRRRR : ',error);
+      res.status(500).send("Terjadi kesalahan pada server");
+    }
+  },
+
+  updateSEO: async (req, res) => {
+    try {
+ 
+      const id = 1;
+      const { tagManager, googleTagManager, searchConsole, googleAnalytics, token } = req.body;
+  
+      let settingResponse = await axios.put(
+        `${process.env.baseUrl}/admin/${id}/seo/update`,
+        { tagManager, googleTagManager, searchConsole, googleAnalytics },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          }
+        }
+      );
+  
+      if (settingResponse.status === 200) {
+        res.render('seo', {
+          successMessage: 'Category updated successfully',
+          dataSetting: settingResponse.data
+        });
+      } else {
+        res.render('seo', {
+          errorMessage: 'Failed to update category',
+          dataSetting: req.body
+        });
+      }
+    } catch (error) {
+      console.error('Error updating category:', error.message);
+      res.render('seo', {
+        errorMessage: 'Internal Server Error',
+        dataSetting: req.body
+      });
+    }
+  },
 };
--- routes/setting.js
+++ routes/setting.js
@@ -4,8 +4,11 @@ const settingControllers = require("../controllers/SettingController");
 router.get('/setting', settingControllers.viewSetting);
 router.post('/admin/:id/setting/update', settingControllers.updateSetting);
 
-router.get('/socialmedia', settingControllers.viewSosmed);
-router.get('/seo', settingControllers.viewSeo);
+router.get('/social/media', settingControllers.viewSosmed);
+router.put('/admin/:id/social/media/update', settingControllers.updateSosmed);
+
+router.get('/seo', settingControllers.viewSEO);
+router.post('/admin/:id/seo/update', settingControllers.updateSEO);
 
 
 module.exports = router;
